ede7c43f3eff6643c20cb596ad8a8dbe672d0920
Yashraj1118 <yash.raj17182003@gmail.com>
2025-07-01 09:56:48 +0530

    Complete merge

--- Day2(Bridgelabz)/handshake.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class handshake {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int totalstudents= sc.nextInt();
-		int totalhandshake=(totalstudents*(totalstudents-1))/2;
-        System.out.println( "The total no of handshake is "+ totalhandshake);
-    }
-}
